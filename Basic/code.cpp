908c216c516c66e6a9a0c03f9ba4a4f4560d67ec
Darshan kyada <darshankyada02@gmail.com>
2025-06-05 11:54:20 +0530

    DSA basic solution and problem solving question

---
+++ Basic/code.cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "darshan \nkyada" << "\n";
+
+    float pi = 3.14;
+    cout << pi;
+
+    // type castig conversion
+    char grade = 'a';
+    int value = grade;
+    cout << value << endl;
+
+    // type casting casting
+    double price = 100.99;
+    int newPrice = (int)price;
+    cout << newPrice << endl;
+
+    // input in c++
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // cout << "your age is: " << age << "\n";
+
+    // logical operater ! not
+    cout << !(3 < 1) << endl;
+
+    // || OR operator
+    cout << ((3 < 1) || (4 > 5)) << endl;
+
+    // Unary Operator
+    int a = 10;
+    int b = a++;
+    // first b in 10 store after a is a++ then a is 11
+    cout << "a is " << a << endl; // 11
+    cout << "b is " << b << endl; // 10
+
+    int c = 10;
+    int d = ++c;
+    // first ++c then c is 11 and d is c then d is 11
+    cout << "c is " << c << endl; // 11
+    cout << "d is " << d << endl; // 11
+
+    //  Example calculator
+    // int abc;
+    // int xyz;
+    // cout << "enter abc value: " << endl;
+    // cin >> abc;
+    // cout << "enter xyz value: " << endl;
+    // cin >> xyz;
+    // cout << "abc + xyz sum of: " << abc + xyz << endl;
+    // cout << "abc * xyz multiply of: " << abc * xyz << endl;
+    // cout << "abc - xyz difference of: " << abc - xyz << endl;
+    // if (xyz != 0)
+    // {
+    //     cout << "abc / xyz divison of: " << abc / xyz << endl;
+    //     cout << "abc % xyz modules of: " << abc % xyz << endl;
+    // }
+    // else
+    // {
+    //     cout << "Divison and module by zero are not allowed" << endl;
+    // }
+
+    //
+    int x = 10;
+    int *p1 = &x;
+    int *p2 = p1;
+    cout << *p2 << endl;
+
+    // sum of odd num 1 to n
+    int n = 50;
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            count += i;
+        }
+    }
+    cout << count << " \n";
+
+    // prime or non prime
+    int q = 4;
+    bool isprime = true;
+    for (int i = 2; i <= q - 1; i++)
+    {
+        if (q % i == 0)
+        {
+            isprime = false;
+            break;
+        }
+    }
+    if (isprime == true)
+    {
+        cout << q << " is a prime number" << endl;
+    }
+    else
+    {
+        cout << q << " is not a prime number" << endl;
+    }
+
+    // calculate the sum of all numbers from 1 to n that are divisible by 3
+    int m = 5;
+    int sumOfAll = 1;
+    for (int i = 1; i <= m; i++)
+    {
+        if (i % 3 == 0)
+        {
+            sumOfAll += i;
+        }
+    }
+    cout << "Sum of all numbers from 1 to " << m << " is: " << sumOfAll << endl;
+
+    int o = 10;
+    int factorial = 1;
+    for (int i = 1; i <= o; i++)
+    {
+        factorial *= i;
+    }
+    cout << factorial << endl;
+    return 0;
+}
---
+++ Basic/pattern.cpp
@@ -0,0 +1,348 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Dk" << endl;
+
+    // Square Pattern
+
+    // int n = 4;
+    // for (int i = 1; i <= n; i++)
+    // {
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         cout << "*" << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1 2 3 4
+    // 1 2 3 4
+    // 1 2 3 4
+    // 1 2 3 4
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A';
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+    // output:
+    // A B C D
+    // A B C D
+    // A B C D
+    // A B C D
+
+    // int a = 3;
+    // int num = 1;
+    // for (int i = 0; i < a; i++)
+    // {
+    //     for (int j = 0; j < a; j++)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1 2 3
+    // 4 5 6
+    // 7 8 9
+
+    // int a = 3;
+    // char ch = 'A';
+    // for (int i = 0; i < a; i++)
+    // {
+    //     for (int j = 0; j < a; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // A B C
+    // D E F
+    // G H I
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // *
+    // * *
+    // * * *
+    // * * * *
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1
+    // 22
+    // 333
+    // 4444
+
+    // int n = 6;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A' + i;
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << ch;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // A
+    // BB
+    // CCC
+    // DDDD
+    // EEEEE
+    // FFFFFF
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << j+1 << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1
+    // 1 2
+    // 1 2 3
+    // 1 2 3 4
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = i + 1; j > 0; j--)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1
+    // 2 1
+    // 3 2 1
+    // 4 3 2 1
+
+    // int n = 4;
+    // int num = 1;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1
+    // 2 3
+    // 4 5 6
+    // 7 8 9 10
+
+    // int n = 4;
+    // char ch = 'A';
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << ch << " ";
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // A
+    // B C
+    // D E F
+    // G H I J
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     char ch = 'A' + i;
+
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         cout << (char)(ch - j) << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // A
+    // B A
+    // C B A
+    // D C B A
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = 0; k < n - i; k++)
+    //     {
+    //         cout << i + 1;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // 1111
+    //  222
+    //   33
+    //    4
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     char ch = 'A';
+    //     for (int k = 0; k < n - i; k++)
+    //     {
+    //         cout << (char)(ch + i);
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    // AAAA
+    //  BBB
+    //   CC
+    //    D
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     // Space
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     // Num1 - Left
+    //     for (int k = 1; k <= i + 1; k++)
+    //     {
+    //         cout << k;
+    //     }
+    //     // Num2- right
+    //     for (int l = i; l > 0; l--)
+    //     {
+    //         cout << l;
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    //    1
+    //   121
+    //  12321
+    // 1234321
+
+    // int n = 4;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < n - i - 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+
+    //     if (i != 0)
+    //     {
+    //         for (int k = 0; k < 2 * i - 1; k++)
+    //         {
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // for (int i = 0; i < n - 1; i++)
+    // {
+    //     for (int j = 0; j < i + 1; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+    //     if (i != n - 2)
+    //     {
+    //         for (int k = 0; k < 2 * (n - i) - 5; k++)
+    //         {
+    //             cout << " ";
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // Output:
+    //    *
+    //   * *
+    //  *   *
+    // *     *
+    //  *   *
+    //   * *
+    //    *
+
+    int n = 4;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 1; k <= 2 * (n - i); k++)
+        {
+            cout << " ";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "*";
+        }
+        for (int k = 1; k <= 2 * (n - i); k++)
+        {
+            cout << " ";
+        }
+        for (int l = 1; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
